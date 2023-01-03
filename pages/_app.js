399c21dd8224b67a811ebb669974263f73c469a8
joseobm92 <joseobm92@gmail.com>
2023-01-03 15:26:06 -0500

    minor fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { Layout } from '../components';
+import { Layout } from "../components";
 
-import '../styles/globals.css';
-import { StateContext } from '../context/StateContext';
-import { Toaster } from 'react-hot-toast';
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
     <StateContext>
-
       <Layout>
-<Toaster />
+        <Toaster />
         <Component {...pageProps} />
-
       </Layout>
     </StateContext>
-  )
+  );
 }
